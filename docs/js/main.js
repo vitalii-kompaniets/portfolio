edde39a1bf20c217a1127ef4f46b70b8991e29ff
Vitalii <vittiger09@gmail.com>
2020-07-12 22:48:50 +0300

    Fifth commit

--- docs/js/main.js
+++ docs/js/main.js
@@ -6,3 +6,21 @@ $(document).ready(function () {
         }
     }
 )});
+
+// fake-placeholder
+const formInputs = document.querySelectorAll('.form-field');
+
+for (let item of formInputs) {
+
+    const inputPlaceholder = item.nextElementSibling;
+
+    item.addEventListener('click', function(){
+        inputPlaceholder.classList.add('active');
+    });
+
+    item.addEventListener('blur', function() {
+        if(this.value == '') {
+        inputPlaceholder.classList.remove('active');
+        }
+    })
+}
