15ee868638c848a48427d4e17c805f86f2cea895
Vitalii <vittiger09@gmail.com>
2020-07-19 14:44:34 +0300

    Alfa commit

--- docs/js/main.js
+++ docs/js/main.js
@@ -5,9 +5,28 @@ $(document).ready(function () {
             block: ""
         }
     }
-)});
+)
 
-// fake-placeholder
+    // Mobile Menu
+    const mobileMenuToggle = document.querySelector('.toggle-menu');
+    const mobMenu = document.querySelector('.header-menu');
+    const overlay = document.querySelector('#overlay');
+
+    mobileMenuToggle.addEventListener('click', function(){
+        mobMenu.classList.toggle('active');
+        this.classList.toggle('active');
+        overlay.classList.toggle('active');
+    });
+
+    window.addEventListener('resize', function(){
+        mobMenu.classList.remove('active');
+        mobileMenuToggle.classList.remove('active');
+        overlay.classList.remove('active');
+    })
+
+});
+
+// Fake-placeholder
 const formInputs = document.querySelectorAll('.form-field');
 
 for (let item of formInputs) {
